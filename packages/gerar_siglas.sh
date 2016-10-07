1995ed669f6759a387293a03ce09dcc9441730fb
João Batista <joao42lbatista@gmail.com>
2016-10-06 23:32:16 -0300

    Corrected the scripts

--- packages/gerar_siglas.sh
+++ packages/gerar_siglas.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+cd ..
 makeindex dissertacao_Joao_Batista.nlo -s nomencl.ist -o dissertacao_Joao_Batista.nls
+cd -
 #
--- packages/removeLogFileLatex.sh
+++ packages/removeLogFileLatex.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+cd ..
 rm *.aux *.bbl *.blg *.brf *.glo *.idx *.ilg *.ist *.lof *.log *.lot *.mw *.nlo *.nls *.synctex.gz *.toc
+cd -
 #
