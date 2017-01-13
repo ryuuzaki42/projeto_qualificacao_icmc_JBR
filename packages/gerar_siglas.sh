f8232acb5b811e843ff5a6fa8d0eb6f8fa1123b1
João Batista <joao42lbatista@gmail.com>
2017-01-13 12:52:15 -0200

    w1

--- packages/gerar_siglas.sh
+++ packages/gerar_siglas.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 cd ..
 makeindex dissertacao_Joao_Batista.nlo -s nomencl.ist -o dissertacao_Joao_Batista.nls
-cd -
-#
--- packages/removeLogFileLatex.sh
+++ packages/removeLogFileLatex.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 cd ..
 rm *.aux *.bbl *.blg *.brf *.glo *.idx *.ilg *.ist *.lof *.log *.lot *.mw *.nlo *.nls *.synctex.gz *.toc
-cd -
-#
